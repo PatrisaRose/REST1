0557f8d7601910743dd4f37f3e8181f75d3e4900
PatrisaRose <cseresznyevirag@4gmail.com>
2023-11-11 12:26:54 +0100

    form hozzáadása

--- View/Urlap/UrlapView.js
+++ View/Urlap/UrlapView.js
@@ -1,16 +1,21 @@
 import { AdatLeiro } from "../AdatLeiro.js";
 
-export default class UrlapView{
-    constructor(szuloELem){
-        this.szuloELem = szuloELem;
-        this.htmlOsszerak();
-    }
-    htmlOsszerak(){
-        let txt = "<form>"
-        txt += `
-        <label>${this.AdatLeiro[key]}</label>˙
-        `
-        txt += `<button type="submit" class="btn btn-default">Submit</button>`
-        txt += "</form>"
-    }
+export default class UrlapView {
+  constructor(szuloELem) {
+    this.AdatLeiro = AdatLeiro;
+    this.szuloELem = szuloELem;
+    this.htmlOsszerak();
+  }
+  htmlOsszerak() {
+    let txt = "<form>";
+    txt += `
+        <div class="form-group">
+        <label for="id">${this.AdatLeiro[key]}</label>
+        <input type="number" class="form-control" id="id">
+        </div>
+        `;
+    txt += `<button type="submit" class="btn btn-default">Submit</button>`;
+    txt += "</form>";
+    this.szuloELem.append(txt);
+  }
 }
